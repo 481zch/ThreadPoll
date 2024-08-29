25ad0b5c18b0a96de30dd90cd132d70bd1bf20d1
481zch <3310560049@qq.com>
2024-08-29 22:22:31 +0800

    Update ThreadPool.h

--- ThreadPool.h
+++ ThreadPool.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <list>
 #include <mutex>
 #include <queue>
 #include <functional>
@@ -8,11 +9,32 @@
 #include <vector>
 #include <condition_variable>
 #include <initializer_list>
+#include <unordered_map>
+
+
+#define MIN_THREADS 4
+#define MAX_THREADS 40
+#define DEFAULT_THREADS 4
+#define DEFAULT_INTERVAL 1000
+
+class Task {
+public:
+    std::function<void()> func;
+    unsigned int priority;
+
+public:
+    Task() = default;
+    Task(std::function<void()> f, unsigned int p) : func(std::move(f)), priority(p) {}
+
+    bool operator<(const Task& other) const {
+        return priority < other.priority;
+    }
+};
 
 template <typename T>
 class SafeQueue {
 private:
-    std::queue<T> m_queue;
+    std::priority_queue<T, std::vector<T>, std::less<>> m_queue;
     std::mutex m_mutex;
 
 public:
@@ -21,6 +43,7 @@ public:
         std::lock_guard<std::mutex> lock(other.m_mutex);
         m_queue = std::move(other.m_queue);
     }
+
     ~SafeQueue() {}
 
     bool empty() {
@@ -35,14 +58,14 @@ public:
 
     void enqueue(T&& t) {
         std::unique_lock<std::mutex> lock(m_mutex);
-        m_queue.emplace(std::forward<T>(t));
+        m_queue.push(std::forward<T>(t));
     }
 
     bool dequeue(T& t) {
         std::unique_lock<std::mutex> lock(m_mutex);
         if (m_queue.empty())
             return false;
-        t = std::move(m_queue.front());
+        t = std::move(m_queue.top());
         m_queue.pop();
         return true;
     }
@@ -52,65 +75,151 @@ class ThreadPool {
 private:
     class ThreadWorker {
     private:
-        int m_id;
         ThreadPool* m_pool;
 
     public:
-        ThreadWorker(ThreadPool* pool, const int id) : m_pool(pool), m_id(id) {}
+        ThreadWorker(ThreadPool* pool) : m_pool(pool) {}
 
         void operator()() {
-            std::function<void()> func;
+            Task t;
             bool dequeued;
             while (!m_pool->m_shutdown) {
                 {
                     std::unique_lock<std::mutex> lock(m_pool->m_conditional_mutex);
                     m_pool->m_conditional_lock.wait(lock, [this] { return !m_pool->m_queue.empty() || m_pool->m_shutdown; });
                     if (m_pool->m_shutdown) break;
-                    dequeued = m_pool->m_queue.dequeue(func);
+                    dequeued = m_pool->m_queue.dequeue(t);
                 }
-                if (dequeued) func();
+                if (dequeued)
+                {
+                    ++m_pool->work_nums;
+                    --m_pool->sleep_nums;
+                    t.func();
+                    --m_pool->work_nums;
+                    ++m_pool->sleep_nums;
+                }
+                // 在没有任务并且被通知了，说明要缩减线程
+                else
+                {
+                    std::unique_lock<std::mutex> lock(m_pool->m_mutex);
+                    std::thread::id this_id = std::this_thread::get_id();
+                    if (m_pool->mp.count(this_id))
+                    {
+                        m_pool->lst_threads.erase(m_pool->mp[this_id]);
+                        m_pool->mp.erase(this_id);
+                        --m_pool->sleep_nums;
+                    }
+                    return;
+                }
             }
         }
     };
 
     bool m_shutdown;
-    SafeQueue<std::function<void()>> m_queue;
-    std::vector<std::thread> m_threads;
+    SafeQueue<Task> m_queue;
     std::mutex m_conditional_mutex;
+    std::mutex m_mutex;
     std::condition_variable m_conditional_lock;
+    size_t max_threads;
+    size_t min_threads;
+    // 线程id和链表迭代器
+    std::unordered_map<std::thread::id, std::list<std::thread>::iterator> mp;
+    std::list<std::thread> lst_threads;
+    std::atomic<int> work_nums;
+    std::atomic<int> sleep_nums;
+    // 引入定时器机制和定时器线程
+    std::chrono::milliseconds timer_interval;
+    std::thread timer_thread;
 
 public:
-    ThreadPool(const int n_threads = 4) : m_threads(std::vector<std::thread>(n_threads)), m_shutdown(false) {}
+    ThreadPool(const int n_threads = DEFAULT_THREADS, const size_t min_threads = MIN_THREADS, const size_t max_threads = MAX_THREADS, std::chrono::milliseconds interval = std::chrono::milliseconds(DEFAULT_INTERVAL)) : lst_threads(std::list<std::thread>(n_threads)), m_shutdown(false), min_threads(min_threads), max_threads(max_threads), timer_interval(interval)
+    {
+        work_nums = 0;
+        sleep_nums = n_threads;
+        timer_thread = std::thread(&ThreadPool::timer_function, this);
+    }
 
     ~ThreadPool() {
         if (!m_shutdown) shutdown();
     }
 
     void init() {
-        for (int i = 0; i < m_threads.size(); ++i) {
-            m_threads.at(i) = std::thread(ThreadWorker(this, i));
+        for (auto it = lst_threads.begin(); it != lst_threads.end(); ++it) {
+            *it = std::thread(ThreadWorker(this));
+            mp[it->get_id()] = it;
         }
     }
 
     void shutdown() {
         m_shutdown = true;
         m_conditional_lock.notify_all();
-        
-        for (auto& thread : m_threads) {
-            if (thread.joinable()) {
-                thread.join();
+        if (timer_thread.joinable()) {
+            timer_thread.join();
+        }
+        for (auto& it : lst_threads) {
+            if (it.joinable()) {
+                it.join();
             }
         }
     }
 
     template <typename F, typename... Args>
-    auto submit(F&& f, Args&&... args) -> std::future<decltype(f(args...))> 
-    {
-        std::function<decltype(f(args...))()> func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+    auto submit(unsigned int priority, F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+        auto func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
         auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
-        std::function<void()> wrapper_func = [task_ptr]() { (*task_ptr)(); };
-        m_queue.enqueue(std::move(wrapper_func));
+        Task task([task_ptr]() { (*task_ptr)(); }, priority);
+        m_queue.enqueue(std::move(task));
         m_conditional_lock.notify_one();
         return task_ptr->get_future();
     }
+
+    template <typename F, typename... Args>
+    auto submit(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+        return submit(0, std::forward<F>(f), std::forward<Args>(args)...);
+    }
+
+private:
+    void resizePool();
+    void timer_function() {
+        while (!m_shutdown) {
+            std::this_thread::sleep_for(timer_interval);  // 定时器间隔
+            resizePool();  // 调整线程池大小
+        }
+    }
 };
+
+void ThreadPool::resizePool()
+{
+    std::unique_lock<std::mutex> lock(m_mutex);
+    size_t task_num = m_queue.size();
+    size_t cur_threads = lst_threads.size();
+
+    // 扩展线程池
+    if (task_num > work_nums && cur_threads < max_threads)
+    {
+        size_t add_threads = std::min(max_threads - cur_threads, task_num - work_nums);
+        for (size_t i = 0; i < add_threads; ++i)
+        {
+            auto it = lst_threads.emplace(lst_threads.end(), std::thread(ThreadWorker(this)));
+            mp[it->get_id()] = it;
+            ++sleep_nums;
+        }
+        return;
+    }
+
+    // 收缩线程池
+    if (work_nums < sleep_nums && cur_threads > min_threads)
+    {
+        size_t remove_threads = cur_threads - std::max(2 * task_num, (size_t)min_threads);
+        size_t temp = std::min((size_t)sleep_nums, cur_threads - min_threads);
+        remove_threads = std::min(temp, remove_threads);
+
+        for (size_t i = 0; i < remove_threads; ++i)
+        {
+            m_conditional_lock.notify_one();
+        }
+        return;
+    }
+}
+
+
